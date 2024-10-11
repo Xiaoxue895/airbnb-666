cd983a4a3efe84f6f3ea3d217b5350e925d3cf79
Xiaoxue <xiaoxuew895@gmail.com>
2024-10-11 16:40:21 -0700

    phase3 line 430

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
+import Navigation from '../components/Navigation';
 
 import * as sessionActions from '../store/session';
 
@@ -20,6 +21,7 @@ function Layout() {
 
   return (
     <>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
