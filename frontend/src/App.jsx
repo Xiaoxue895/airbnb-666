04d2fab71a4c1f2a9297da0254aa9769c5a17f72
Xiaoxue <xiaoxuew895@gmail.com>
2024-10-13 21:51:39 -0700

    finish phase4 line 752

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,11 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from '../components/LoginFormPage';
-import SignupFormPage from '../components/SignupFormPage';
-import Navigation from '../components/Navigation';
+// import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
 
-import * as sessionActions from '../store/session';
+import * as sessionActions from './store/session';
 
 function Layout() {
   const dispatch = useDispatch();
@@ -35,10 +35,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <h1>Welcome!</h1>
       },
-      {
-        path: '/login',
-        element: <LoginFormPage />
-      },
+      // {
+      //   path: '/login',
+      //   element: <LoginFormPage />
+      // },
       {
         path: "/signup",
         element: <SignupFormPage />
